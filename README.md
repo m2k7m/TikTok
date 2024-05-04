03cf7a6159de084e3a3d95ba23a968288e63eb79
m2k7m <106227973+m2k7m@users.noreply.github.com>
2024-05-04 03:10:17 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,48 +1,26 @@
 
-# Tiktok account info v1
+<br>
+<h1>TikTok Account Info</h1>
+<h2 style="border-bottom: none;">Useful script for extracting TikTok account information</h2>
 
-اداة تستخرج معلومات اضافية لحسابات التيك توك
+<h2>Requirements:</h2>
 
-## Authors
-
-- Telegram [@givtt](https://t.me/givtt)
-- Instagram [@we62](https://https://instagram.com/we62)
-
-
-## صورة للأداة
-
-![test](https://github.com/givtt/tiktok-info/assets/126427636/09ae83af-cdc9-4b30-9ce0-bcd77044554f)
-
-
-## البيانات المستخرجة :
-
-البيانات اللتي تقوم الأداة بأستخراجها:
-
-- استخراج [ User ID ] استخراج الايدي الخاص بالحساب
-- استخراج [ NickName ] استخراج الاسم الخاص بالحساب
-- استخراج [ verified ] التحقق اذا حساب موثق
-- استخراج [ private ] التحقق اذا حساب خاص
-- استخراج [ secUid ] استخراج secUid الخاص بالحساب
-- استخراج [ Followers ] استخراج عدد المتابعين
-- استخراج [ Following ] استخراج عدد المتابعون
-- استخراج [ Likes ]     استخراج عدد اللايكات
-- استخراج [ Video Count ] استخراج عدد الفديوهات
-- استخراج [ Open Favorite ] التحقق من المفضلة عامة او خاصة
-- استخراج [ Can See Following list ] التحقق من المتابعون اذا يمكنك مشاهدتهم
-- استخراج [ User Language ] استخراج اللغة الخاصة بالمستخدم
-- استخراج [ User Create Time ] استخراج تاريخ انشاء الحساب ( غير دقيق )
-- استخراج [ Last Time Change Nickname ] استخراج اخر مره تم تغير فيها الاسم ( دقيق جدا )
-- استخراج [ Account Region ] استخراج دولة الحساب
-
-
-
-## التثبيت
-
-- الأداة تتدعم جميع الأجهزة ! ( تأكد من تثبيت بايثون والمكتبات المطلوبة ) :
-- يلزم تثبيت مكتبتين مهمتين :
+Python 3.12.x or higher
 
 ```bash
-  pip install requests
-  pip install bs4
+pip install requests bs4 countryflag
 ```
-    
+
+Make sure to download them all.
+
+<h2>How to use:</h2>
+<ul>
+    <li>Install requirements</li>
+    <li>Download as ZIP</li>
+    <li>Run main.py</li>
+</ul>
+
+<h2 style="border-bottom: none;">Or try it on Replit:</h2>
+<a href="https://replit.com/@MahdiKM1/TikTokAccountInfo#main.py"><img src="https://binbashbanana.github.io/deploy-buttons/buttons/remade/replit.svg" alt="replit"></a>
+
+<h2 style="border-bottom: none;">All credits go to <a href="https://github.com/givtt">givtt</a>, I just modified it a little bit.</h2>
--- main.py
+++ main.py
@@ -1,35 +1,19 @@
-import requests
+
+#pip install requests bs4 countryflag
+
+import requests,json,time,countryflag,os
 from bs4 import BeautifulSoup
 from datetime import datetime
-import json, time
 
-print("""
-
-                 ,  ,
-              #▄▓██████▀
-            "▀███████████▄L
-           ▄R████████████▄▄
-            ▄▀█████████▓▀▀N
-             ' ▀█▀███▀█ ▀
-                ' ▀█▌"
-                   ▐█
-                   ██
-                   ██
-    ""▀▀▀██▄▄   ▄▄▄██▄a▄▄   ,▄▄██▀▀""
-           "▀██▄⌠▀▀▀▀▀'¡▄██▀"
-               ▀██▄  ▄██▀`
-                  ████"
-               ▄▄█▌▀╙██▄▄
-          ██▄▄██▀█    █▀███▀█▌
-
-     [ Made By GIVT . ]
-     [ + ] Telegram : givtt
-     [ + ] Instagram : we62
-     [ ! ] You are not entitled to sell the Tool [ ! ]
-""")
-time.sleep(2)
-
-class LordGivt:
+def format_number(num):
+    if num >= 1000000:
+        return str(num // 1000000) + 'M'
+    elif num >= 1000:
+        return str(num // 1000) + 'K'
+    else:
+        return str(num)
+    
+class TikTok:
     def __init__(self, username: str):
         self.username = username
         self.json_data = None
@@ -44,7 +28,6 @@ class LordGivt:
         self.output()
 
     def send_request(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
         headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 GIVT"}
         r = requests.get(f"https://www.tiktok.com/@{self.username}", headers=headers)
 
@@ -53,70 +36,15 @@ class LordGivt:
             script_tag = soup.find('script', {'id': '__UNIVERSAL_DATA_FOR_REHYDRATION__'})
             script_text = script_tag.text.strip()
             self.json_data = json.loads(script_text)["__DEFAULT_SCOPE__"]["webapp.user-detail"]["userInfo"]
+            with open("file.json","w")as f:
+                json.dump(self.json_data,f,indent=2)
         except:
             input("[X] Error : Username Not Found .")
             exit()
 
-    def get_user_id(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            return str(self.json_data["user"]["id"])
-        except IndexError:
-            return "Unknown"
-
-    def get_name(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            return self.json_data["user"]["nickname"]
-        except IndexError:
-            return "Unknown"
-
-    def is_verified(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            check = self.json_data["user"]["verified"]
-            if check == "false" or check is False:
-                return "No"
-            else:
-                return "Yes"
-        except:
-            return "Unknown"
-
-    def secUid(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            return self.json_data["user"]["secUid"]
-        except:
-            return "Unknown"
-
-    def is_private(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            check = self.json_data["user"]["privateAccount"]
-            if check == "true" or check is True:
-                return "Yes"
-            else:
-                return "No"
-        except:
-            return "Unknown"
-
-    def followers(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            return self.json_data["stats"]["followerCount"]
-        except:
-            return "Unknown"
-
-    def following(self):
-        try:
-            return self.json_data["stats"]["followingCount"]
-        except:
-            return "Unknown"
-
     def user_create_time(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
         try:
-            url_id = int(self.get_user_id())
+            url_id = int(str(self.json_data["user"]["id"]))
             binary = "{0:b}".format(url_id)
             i = 0
             bits = ""
@@ -130,40 +58,28 @@ class LordGivt:
             return "Unknown"
 
     def last_change_name(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            time = self.json_data["user"]["nickNameModifyTime"]
-            check = datetime.fromtimestamp(int(time))
-            return check
-        except:
-            return "Unknown"
-
-    def account_region(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            return self.json_data["user"]["region"]
-        except:
-            return "Unknown"
-
-    def video_count(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            return self.json_data["stats"]["videoCount"]
-        except:
-            return "Unknown"
-
-    def open_favorite(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            check = self.json_data["user"]["openFavorite"]
-            if check is False or check == "false":
-                return "No"
-            return "Yes"
-        except:
-            return "Unknown"
+        if self.json_data["user"]["nickNameModifyTime"] == 0:
+            return "User Never Changed Name"
+        else:
+            try:
+                time = self.json_data["user"]["nickNameModifyTime"]
+                check = datetime.fromtimestamp(int(time))
+                return check
+            except:
+                return "Unknown"
+        
+    def last_change_user(self):
+        if self.json_data["user"]["uniqueIdModifyTime"] == 0:
+            return "User Never Changed Username"
+        else:
+            try:
+                time = self.json_data["user"]["uniqueIdModifyTime"]
+                check = datetime.fromtimestamp(int(time))
+                return check
+            except:
+                return "Unknown"
 
     def see_following(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
         try:
             check = str(self.json_data["user"]["followingVisibility"])
             if check == "1":
@@ -172,42 +88,55 @@ class LordGivt:
         except:
             return "Unknown"
 
-    def language(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            return str(self.json_data["user"]["language"])
-        except:
-            return "Unknown"
-
-    def heart_count(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        try:
-            return str(self.json_data["stats"]["heart"])
-        except:
-            return "Unknown"
-
     def output(self):
-        "لا تحط حقوقك وتكتب برمجتك ياسبيكة هههههه"
-        print(f"[ Get Info For @{self.username} ] ..\n\n")
-        print(f"[ + ] UserID : {self.get_user_id()}")
-        print(f"[ + ] Nickname : {self.get_name()}")
-        print(f"[ + ] is verified : {self.is_verified()}")
-        print(f"[ + ] is private : {self.is_private()}")
-        print(f"[ + ] secUid : {self.secUid()}")
-        print(f"[ + ] Followers : {self.followers()}")
-        print(f"[ + ] Following : {self.following()}")
-        print(f"[ + ] Likes : {self.heart_count()}")
-        print(f"[ + ] Video Count : {self.video_count()}")
-        print(f"[ + ] Open Favorite : {self.open_favorite()}")
-        print(f"[ + ] Can See Following list : {self.see_following()}")
-        print(f"[ + ] User Language : {self.language()}")
-        print(f"[ + ] User Create Time : {self.user_create_time()}")
-        print(f"[ + ] Last Time Change Nickname : {self.last_change_name()}")
-        print(f"[ + ] Account Region : {self.account_region()}\n\n")
+        print(f"[/] Get TikTok Info For @{self.username}..")
+        time.sleep(2)
+        response = ""
+        if self.json_data["user"].get("uniqueId"):
+            response += f"[+] Username: {self.json_data['user']['uniqueId']}\n"
+        if self.json_data["user"].get("id"):
+            response += f"[+] UserID: {self.json_data['user']['id']}\n"
+        if self.json_data["user"].get("nickname"):
+            response += f"[+] Nickname: {self.json_data['user']['nickname']}\n"
+        if self.json_data["user"].get("signature"):
+            response += f"[+] Bio:\n{self.json_data['user']['signature']}\n"
+        if self.json_data["user"].get("verified") is not None:
+            response += f"[+] Is Verified: {'Yes' if self.json_data['user']['verified'] else 'No'}\n"
+        if self.json_data["user"].get("privateAccount") is not None:
+            response += f"[+] Is Private: {'Yes' if self.json_data['user']['privateAccount'] else 'No'}\n"
+        if self.json_data["stats"].get("followerCount"):
+            response += f"[+] Followers: {format_number(self.json_data['stats']['followerCount'])}\n"
+        if self.json_data["stats"].get("followingCount"):
+            response += f"[+] Following: {format_number(self.json_data['stats']['followingCount'])}\n"
+        if self.json_data["stats"].get("friendCount"):
+            response += f"[+] Friends: {format_number(self.json_data['stats']['friendCount'])}\n"
+        if self.json_data["stats"].get("heart"):
+            response += f"[+] Likes: {format_number(self.json_data['stats']['heart'])}\n"
+        if self.json_data["stats"].get("videoCount"):
+            response += f"[+] Video Count: {format_number(self.json_data['stats']['videoCount'])}\n"
+        if self.see_following():
+            response += f"[+] Can See Following list: {self.see_following()}\n"
+        if self.json_data["user"].get("openFavorite") is not None:
+            response += f"[+] Open Favorite: {'Yes' if self.json_data['user']['openFavorite'] else 'No'}\n"
+        if self.json_data["user"].get("language"):
+            response += f"[+] User Language: {self.json_data['user']['language']}\n"
+        if self.user_create_time() and self.user_create_time() != "Unknown":
+            response += f"[+] User Create Time: {self.user_create_time()}\n"
+        if self.last_change_name() and self.last_change_name() != "Unknown":
+            response += f"[+] Last Time Change Nickname: {self.last_change_name()}\n"
+        if self.last_change_user() and self.last_change_user() != "Unknown":
+            response += f"[+] Last Time Change Username: {self.last_change_user()}\n"
+        if self.json_data["user"].get("region"):
+            response += f"[+] Account Region: {self.json_data['user']['region']} - {countryflag.getflag([f'{self.json_data['user']['region']}'])}\n"
+        if self.json_data["user"].get("avatarLarger"):
+            response += f"[+] Avatar Link: {self.json_data['user']['avatarLarger']}\n"
+
+        print(response)
         input("[ + ] Done Sir ..")
         exit()
 
+os.system('cls' if os.name == 'nt' else 'clear')
 print("\n")
 username = input("[?] Please Enter Username : ")
-print("\n")
-LordGivt(username=username)
+os.system('cls' if os.name == 'nt' else 'clear')
+TikTok(username=username)
